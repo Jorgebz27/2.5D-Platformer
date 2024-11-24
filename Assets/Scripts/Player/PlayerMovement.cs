e8b156d944a21d2b46970118f61d84c6ab87052a
alocue03 <alonsocueog@gmail.com>
2024-11-24 14:07:34 -0600

    player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -174,10 +174,13 @@ public class PlayerMovement : MonoBehaviour
     
     private void Flip()
     {
-        isFacingR = !isFacingR;
-        Vector3 scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
+        if (Time.timeScale == 1)
+        {
+            isFacingR = !isFacingR;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
     }
 
     private void OnDrawGizmosSelected()
